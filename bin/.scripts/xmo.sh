68f9d8a5efd2b84ab1c4e9381f401323121aefc1
Gazaunga <Gazaunga@gmail.com>
2017-03-01 09:54:11 -0500

    Update xmo.sh

--- bin/.scripts/xmo.sh
+++ bin/.scripts/xmo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ~/
 wget https://raw.githubusercontent.com/Gazaunga/Scripts/master/bin/.scripts/zetup.sh
+#wget https://raw.githubusercontent.com/Gazaunga/Scripts/master/bin/.scripts/font.sh
 sudo pacman -Syu --noconfirm -- needed
 cd ~/
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
@@ -16,3 +17,5 @@ sudo pip install rainbowstream glances ImageScraper
 cd ~/
 sudo chmod +x zetup.sh
 ./zetup.sh
+sudo chmod +x font.sh
+./font.sh

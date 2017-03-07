a0f7670aca550361385c9ea81a8066a2cd87f6be
Gazaunga <Gazaunga@gmail.com>
2017-03-07 11:47:00 -0500

    Update mouse.sh

--- bin/.scripts/mouse.sh
+++ bin/.scripts/mouse.sh
@@ -1,21 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
-while :; do
-    case $1 in
-        -e|--enable)
-            xinput set-int-prop $(xinput | awk '/Mouse/ {printf "%s",$9}' | sed 's/id=//') "Device Enabled" 8 1
-            break
-            ;;
-        -d|--disable)
-            xdotool mousemove $(xrandr | awk '/Screen 0/ {printf "%s",$8 " " $10}' | sed 's/,$//')
-            xinput set-int-prop $(xinput | awk '/Mouse/ {printf "%s",$9}' | sed 's/id=//') "Device Enabled" 8 0
-            break
-            ;;
-        *)
-            echo "Usage: ./mouse [option]:
+# File       : /home/wildefyr/.fyre/scripts/mouse
+# Maintainer : Wildefyr | http://wildefyr.net
+# Copyright  : Wildefyr | Licensed under the WTFPL license.
+
+case $1 in
+    -e|--enable)
+        wmp 870 1040
+        xinput set-int-prop $(xinput | awk '/Mouse/ {printf "%s",$9}' | sed 's/id=//') "Device Enabled" 8 1
+        break
+        ;;
+    -d|--disable)
+        ROOT=$(lsw -r); SW=$(wattr w $ROOT); SH=$(wattr h $ROOT); wmp $SW $SH
+        xinput set-int-prop $(xinput | awk '/Mouse/ {printf "%s",$9}' | sed 's/id=//') "Device Enabled" 8 0
+        break
+        ;;
+    *)
+        echo "Usage: ./mouse [option]:
 
 -e | --enable: Enable Mouse
 -d | --disable: Disable Mouse"
-            break
-    esac
-done
+        break
+esac

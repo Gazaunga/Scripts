6946537be466279343d9f4f18501c82998f10d00
Gazaunga <Gazaunga@gmail.com>
2017-03-09 19:38:19 -0500

    Update SymLink.sh

--- bin/.scripts/SymLink.sh
+++ bin/.scripts/SymLink.sh
@@ -7,7 +7,7 @@ bak=~/.bak
 dotfiles=".bashrc .bash_aliases .bash_functions .Xresources .Xdefaults utils.sh"
 folders="emacs"
 
-read -r -p "Symlink ".bashrc .bash_aliases .bash_functions .Xresources .Xdefaults utils.sh? [y/N] " response
+read -r -p "Symlink ".bashrc .editorconfig .fehbg .interrobang .lesskey .profile .screenrc .xbindkeysrc .xinitrc .bash_aliases .bash_functions .Xresources .Xdefaults utils.sh? [y/N] " response
 response=${response,,}
 if [[ $response =~ ^(yes|y)$ ]]; then
 echo "Storing dotfiles at $bak ..."
@@ -38,5 +38,11 @@ if [[ $response =~ ^(yes|y)$ ]]; then
   fi
   ln -sf $dir/nvim ~/.config/nvim
 fi
+
+mv .config ~/.config
+mv .ssh ~/.ssh
+mv bin ~/bin
+mv rofi ~/rofi
+mv terminator ~/terminator
 
 echo "Done successfully!"

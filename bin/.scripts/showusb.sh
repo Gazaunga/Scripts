284e41e2a9862766e98f276d380639c34b20b1ca
Gazaunga <Gazaunga@gmail.com>
2017-05-10 12:24:56 -0400

    Create showusb.sh

---
+++ bin/.scripts/showusb.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+find /path/to/drive -type f -exec file -b '{}' \; -printf '%s\n' | awk -F , 'NR%2 {i=$1} NR%2==0 {a[i]+=$1} END {for (i in a) printf("%12u %s\n",a[i],i)}' | sort -nr
+# create a visual report of a usb drive
